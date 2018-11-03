55a803d16752f8a5bffdd7fc125d70587c737f71
rpasricha45 <rpasricha45@gmail.com>
2018-11-03 16:57:49 -0400

    styling

--- MyReactNative/App.js
+++ MyReactNative/App.js
@@ -1,66 +1,47 @@
-import React, { Component } from 'react'
-import { AppRegistry, View, StyleSheet } from 'react-native'
-
-import Toggle from './Hello'
-
-export default class App extends Component {
-
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Hello from './Hello';
+export default class App extends React.Component {
+  // todo : create a state
   state = {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-  }
-
+  flexDirection: 'row',
+  justifyContent: 'center',
+  alignItems: 'center',
+}
   render() {
-    const {flexDirection, alignItems, justifyContent} = this.state
-    const layoutStyle = {flexDirection, justifyContent, alignItems}
 
-    const primaryAxis = flexDirection === 'row' ? 'Horizontal' : 'Vertical'
-    const secondaryAxis = flexDirection === 'row' ? 'Vertical' : 'Horizontal'
+
+    // put the constants
+
+    const {flexDirection, alignItems, justifyContent} = this.state
+  const layoutStyle = {flexDirection, justifyContent, alignItems}
+
+  const primaryAxis = flexDirection === 'row' ? 'Horizontal' : 'Vertical'
+  const secondaryAxis = flexDirection === 'row' ? 'Vertical' : 'Horizontal'
 
     return (
-      <View style={styles.container}>
-        <Toggle
-          label={'Primary axis (flexDirection)'}
-          value={flexDirection}
-          options={['row', 'column']}
-          onChange={(option) => this.setState({flexDirection: option})}
-        />
-        <Toggle
-          label={primaryAxis + ' distribution (justifyContent)'}
-          value={justifyContent}
-          options={['flex-start', 'center', 'flex-end', 'space-around', 'space-between']}
-          onChange={(option) => this.setState({justifyContent: option})}
-        />
-        <Toggle
-          label={secondaryAxis + ' alignment (alignItems)'}
-          value={alignItems}
-          options={['flex-start', 'center', 'flex-end', 'stretch']}
-          onChange={(option) => this.setState({alignItems: option})}
-        />
-        <View style={[styles.layout, layoutStyle]}>
-          <View style={styles.box} />
-          <View style={styles.box} />
-          <View style={styles.box} />
-        </View>
+      // <View style={styles.container}>
+
+// todo try to move this text
+<View style = {styles.container}>
+
+        <Hello/>
+
+       <Hello/>
+       <Hello/>
+
+
       </View>
-    )
+    );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#FFA07A',
+    alignItems: 'center',
+    justifyContent: 'center',
+  
   },
-  layout: {
-    flex: 1,
-    backgroundColor: 'rgba(0,0,0,0.05)',
-  },
-  box: {
-    padding: 25,
-    backgroundColor: 'steelblue',
-    margin: 5,
-  },
-})
-
-AppRegistry.registerComponent('App', () => App)
+});
--- MyReactNative/Hello.js
+++ MyReactNative/Hello.js
@@ -1,66 +1,31 @@
-import React, { Component } from 'react'
-import { AppRegistry, View, Text, TouchableOpacity, StyleSheet } from 'react-native'
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
-export default class Toggle extends Component {
 
-  onPress = (option) => {
-    const {onChange} = this.props
-
-    onChange(option)
-  }
-
-  renderOption = (option, i) => {
-    const {value} = this.props
-
-    return (
-      <TouchableOpacity
-        style={[styles.option, option === value && styles.activeOption]}
-        onPress={this.onPress.bind(this, option)}
-        key={i}
-      >
-        <Text style={styles.text}>
-          {option}
-        </Text>
-      </TouchableOpacity>
-    )
-  }
-
+export default class Hello extends React.Component {
   render() {
-    const {label, options} = this.props
-
     return (
-      <View style={styles.container}>
-        <Text style={[styles.text, styles.label]}>
-          {label}
-        </Text>
-        <View style={styles.optionsContainer}>
-          {options.map(this.renderOption)}
-        </View>
-      </View>
-    )
+        // <View style = {styles.container}>
+
+      <Text style = {styles.text}> hello from another Component </Text>
+
+// </View>
+
+    );
+
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flexDirection: 'column',
-    paddingBottom: 20,
-  },
-  text: {
-    fontSize: 14,
-  },
-  label: {
-    padding: 4,
-  },
-  optionsContainer: {
-    flexDirection: 'row',
-    flexWrap: 'wrap'
-  },
-  option: {
-    padding: 4,
-    backgroundColor: 'whitesmoke',
-  },
-  activeOption: {
-    backgroundColor: 'skyblue',
-  },
+      container:{
+        flexDirection:'column',
+
+
+      },
+      text:{
+        fontSize:20,
+        // flexDirection:'rows',
+      }
+
+
 })
